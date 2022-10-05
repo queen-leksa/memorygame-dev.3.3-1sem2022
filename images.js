0e12e3cee1cd292a0feb28c56932c36495ea0d26
Leksa <surnacheva@ithub.ru>
2022-10-05 14:12:36 +0300

    memory game part 1

---
+++ images.js
@@ -0,0 +1,16 @@
+const img = [
+    "images/cloud-drizzle.svg",
+    "images/cloud-drizzle-fill.svg",
+    "images/cloud-haze.svg",
+    "images/cloud-haze-fill.svg",
+    "images/cloud-rain.svg",
+    "images/cloud-rain-fill.svg",
+    "images/cloud-moon.svg",
+    "images/cloud-moon-fill.svg",
+    "images/cloud-sun.svg",
+    "images/cloud-sun-fill.svg",
+    "images/cloudy.svg",
+    "images/cloudy-fill.svg"
+];
+
+const getNumber = (n) => Math.floor(Math.random() * n);
